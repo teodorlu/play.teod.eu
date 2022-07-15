e78c071ffd12324a00cf7e56081deb1217fe39a7
Teodor Heggelund <git@teod.eu>
2022-07-15 17:24:52 +0200

    Stuff

--- document-transform-pandoc-clojure/rickroll.clj
+++ document-transform-pandoc-clojure/rickroll.clj
@@ -1,16 +1,42 @@
 (ns rickroll
-  (:require [clojure.walk]
-            [clojure.edn]))
+  (:require [clojure.walk] ; for recursive transformation
+            [clojure.edn]  ; for reading pandoc JSON as EDN
+            ))
+
+;; there's going to be some line-end overflowing.
+;; Sorry about that!
+;; Consider reading the raw file instead:
+;;
+;;
+
+(comment
+  ;; a nice pattern for recursive transformation in Clojure:
+  (clojure.walk/prewalk (fn [el]
+                          (if (string? el) ; touch strings
+                            (keyword el)   ; do this to strings
+                            el))           ; otherwise let it be
+                        {:big ["nested" "structure"]}) ; big thing in here
+  )
+
+;; Here's the predicate we're going to use later:
 
 (defn pandoc-link?
   "Is this a valid Pandoc link?"
   [pandoc]
   (= "Link" (:t pandoc)))
 
+;; I choose to pull "this is an empty element" out of the walk logic
+
 (defn pandoc-empty
   "Empty Pandoc element"
   []
   {})
+
+;; and I wrote this as an intermediate step.
+;;
+;; take small steps. Ask Geepaw.
+;;
+;; https://www.geepawhill.org/2021/09/29/many-more-much-smaller-steps-first-sketch/
 
 (defn remove-links [pandoc]
   (clojure.walk/prewalk (fn [el]
@@ -19,26 +45,44 @@
                             el))
                         pandoc))
 
+;; Here's the thing!
+
 (defn rickroll [pandoc]
-  (let [_pandoc-link-example {:t "Link",
+  (let [;; I just copied in an example of what I was going to generate
+        _pandoc-link-example {:t "Link",
                       :c [["" [] []]
                           [{:t "Str", :c "teod.eu"}]
                           ["https://www.youtube.com/watch?v=dQw4w9WgXcQ" ""]]}
+        ;; which made the assoc-in okay to write
         link-to-rick (fn [el]
                     (assoc-in el [:c 2 0] "https://www.youtube.com/watch?v=dQw4w9WgXcQ"))]
+    ;; now, just follow the walk pattern from above.
     (clojure.walk/prewalk (fn [el]
                             (if (pandoc-link? el)
                               (link-to-rick el)
                               el))
                           pandoc)))
 
-;; (remove-links *input*)
-;; pandoc -i doc.md --filter "bash -c \"jet --from json --keywordize | bb rickroll.clj | jet --to json\" -o doc-no-links.md
+;; I first tried running it all at once:
+;;
+;;   pandoc -i doc.md --filter "bash -c \"jet --from json --keywordize | bb rickroll.clj | jet --to json\" -o doc-no-links.md
+;;
+;; Sorry about the overflow. I want to fix that!
+;;
+;; Here's a
+;;
+;;
+;;
+;; turns out, pandoc doesn't support this. A filter must be a single script.
+;; Back to this later.
+
+;; I hard-code some example data so that "just running" gives me feedback
 
 (def example
   {:pandoc-api-version [1 22 2], :meta {}, :blocks [{:t "Para", :c [{:t "Str", :c "See"} {:t "Space"} {:t "Link", :c [["" [] []] [{:t "Str", :c "teod.eu"}] ["https://teod.eu" ""]]}]}]})
 
-;; If *in* looks like something we can use, use it.
+
+;; ... but if *in* looks like something we can use, use it!
 
 (def input
   (try
@@ -46,11 +90,10 @@
     (catch RuntimeException _
         ())))
 
-(def transform rickroll)
-
-(if (map? input)
-  (transform input)
-  (transform example))
+(let [transform rickroll] ; I wanted a single place to choose what happens -- remove links or rickroll
+  (if (map? input)
+    (transform input)
+    (transform example)))
 
 ;; Usage:
 ;;

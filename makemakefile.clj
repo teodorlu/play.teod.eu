312672d6a0b61c0144b834f6c131126819451828
Teodor Heggelund <git@teod.eu>
2022-07-24 16:16:06 +0200

    Index depends on metadata

--- makemakefile.clj
+++ makemakefile.clj
@@ -33,7 +33,7 @@
 
 (println "# Generate target for root index")
 ;; TODO root index also depends on all the play.edn files found
-(println (str/join " " (concat ["index.html:" "index.clj"] (map html targets))))
+(println (str/join " " (concat ["index.html:" "index.clj"] (map html targets) (map play-edn targets))))
 (println "\t./index.clj")
 (println "")
 (println "")

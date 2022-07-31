7cac9a95f32bea8e7f83dd2a82c90fe5201153c5
Teodor Heggelund <git@teod.eu>
2022-07-31 11:03:39 +0200

    View stuff

--- scicloj-libraries/libs.cljs
+++ scicloj-libraries/libs.cljs
@@ -1,8 +1,8 @@
-(ns libs
-  (:require
-    [clojure.edn :as edn]
-    [reagent.core :as r]
-    [ajax.core :refer [GET]]))
+(ns libs)
+
+(require '[clojure.edn :as edn]
+         '[reagent.core :as r]
+         '[ajax.core :refer [GET]])
 
 ;; (def data (slurp "https://scicloj.github.io/docs/resources/model.edn"))
 
@@ -14,6 +14,24 @@
 (defn set-raw-response! [response]
   (swap! state assoc :raw-response response))
 
+(defn view [data]
+  [:pre (pr-str data)])
+
+(defn table2 [{}]
+  [:table [:thead [:th "x"] [:th "y"]]
+   [:tbody [:tr [:td 1] [:td 2]]]])
+
+(defn table3 [{:keys [rows header viewers]}]
+  [:table [:thead
+           (for [h header]
+             [:th h])]
+   [:tbody [:tr [:td 1] [:td 2]]
+    (for [r rows]
+      [:tr
+       (for [h header]
+         (let [view (or (h viewers) identity)]
+           [:td (view (h r))]))])]])
+
 (defn table []
   [:div
    [:p "Clacks: " (:clicks @state)]
@@ -22,8 +40,16 @@
    (when-let [response (:raw-response @state)]
      (let [model (edn/read-string response)]
        [:div
-        [:pre (str "hello. Response length: " (count response))]
-        [:pre (pr-str (keys model))]
-        [:pre (pr-str (:teodorlu/questions model))]]))])
+        [view (keys model)]
+        [view (first (:tags model))]
+        [view (ffirst (:libs model))]
+        [table2]
+        [table3 {:header [:lib/name :lib/url :lib/category :tags :star]
+                 :rows (first (:libs model))
+                 :viewers {:star (fn [item]
+                                   (when (= item :star)
+                                     "⭐"))}
+                 }]
+        ]))])
 
 (GET "https://scicloj.github.io/docs/resources/model.edn" {:handler set-raw-response!})

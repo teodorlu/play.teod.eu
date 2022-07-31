552ebf2b9b0e4017a6c21036fe5af6585f99c524
Teodor Heggelund <git@teod.eu>
2022-07-31 12:48:37 +0200

    Shortname

--- scicloj-libraries/libs.cljs
+++ scicloj-libraries/libs.cljs
@@ -2,7 +2,8 @@
 
 (require '[clojure.edn :as edn]
          '[reagent.core :as r]
-         '[ajax.core :refer [GET]])
+         '[ajax.core :refer [GET]]
+         '[clojure.string :as str])
 
 ;; (def data (slurp "https://scicloj.github.io/docs/resources/model.edn"))
 
@@ -20,7 +21,6 @@
 (defn view-table [{:keys [rows header viewers]}]
   [:table [:thead
            (for [h header]
-
              [:th h])]
    [:tbody
     (for [r rows]
@@ -29,21 +29,33 @@
          (let [view (or (h viewers) identity)]
            [:td (view (h r))]))])]])
 
+(defn scicloj-libs-model->table [model]
+  {:header [:lib/name :lib/url #_:lib/category :tags :star]
+   :rows (apply concat (:libs model))
+   :viewers {:star (fn [item]
+                     (when (= item :star)
+                       "⭐"))
+             :lib/url (fn [url]
+                        (when url
+                          (let [shortname (str/replace url #"https://github.com/" "")]
+                            [:a {:href url} shortname])))}})
+
 (defn table []
   [:div
    #_#_
    [:p "Clacks: " (:clicks @state)]
    [:p [:button {:on-click #(swap! state update :clicks inc)}
         "Click me!"]]
+   [:h2 "Plain HTML table"]
    (when-let [response (:raw-response @state)]
      (let [model (edn/read-string response)]
        [:div
-        [view-table {:header [:lib/name :lib/url #_:lib/category :tags :star]
-                 :rows (apply concat (:libs model))
-                 :viewers {:star (fn [item]
-                                   (when (= item :star)
-                                     "⭐"))}
-                 }]
-        ]))])
+        [view-table (scicloj-libs-model->table model)]
+        ]))
+
+   [:h2 "Fancy table"]
+   [:p "try that data thing."]
+
+   ])
 
 (GET "https://scicloj.github.io/docs/resources/model.edn" {:handler set-raw-response!})

6421a1abd9c4325ca6e24523a869b3b2be36dc0e
Teodor Heggelund <git@teod.eu>
2022-08-07 14:04:36 +0200

    New nice problem

---
+++ scicloj-libraries/tool.clj
@@ -0,0 +1,7 @@
+;; idea -- pull down https://scicloj.github.io/docs/resources/model.edn and turn it into static HTML
+;;
+;; downsides -- early bound
+;;
+;; remedy -- datatables as a react component, so that I can load it when I want.
+;;
+;; Decision 2022-08-07 -- try react component for data tables instead, see how that works. It won't need any local updates to keep track.

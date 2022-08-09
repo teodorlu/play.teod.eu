43884651a69d92346890dff1ad3e541cab3a7933
Teodor Heggelund <git@teod.eu>
2022-08-09 13:04:53 +0200

    Pull into js

---
+++ xml-on-the-web/sandbox.js
@@ -0,0 +1,135 @@
+const getXml = () => {
+  return `
+<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:metadata="http://xmlns.escenic.com/2010/atom-metadata">
+
+    <id> http://ece5.snapshot.api.no/dala/webservice/escenic/content/498373 </id> <link rel="self" href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/498373" type="application/atom+xml; type=entry"/> <link rel="http://www.vizrt.com/types/relation/lock" href="http://ece5.snapshot.api.no/dala/webservice/escenic/lock/article/498373/24370377" type="application/atom+xml; type=entry"/> <link href="http://ece5.snapshot.api.no/dala/webservice/escenic/publication/dalane-tidende/" rel="http://www.vizrt.com/types/relation/top" type="application/atom+xml; type=entry" title="dalane-tidende"/> <link href="http://ece5.snapshot.api.no/dala/webservice/escenic/publication/dalane-tidende/" rel="http://www.vizrt.com/types/relation/publication" type="application/atom+xml; type=entry" title="dalane-tidende"/> <link rel="http://www.vizrt.com/types/relation/more-info" href="http://ece5.snapshot.api.no/dala/webservice-extensions/core/article-history/?language=%7Blanguage%7D&amp;locale=%7Blocale%7D&amp;url=http://ece5.snapshot.api.no/dala/webservice/escenic/content/498373/log/" type="application/atom+xml; type=entry" title="History"/>
+
+    <content type="application/vnd.vizrt.payload+xml">
+
+        <vdf:payload xmlns:vdf="http://www.vizrt.com/types" model="http://ece5.snapshot.api.no/dala/webservice/escenic/publication/dalane-tidende/model/content-type/story">
+            <vdf:field name="frontpageTitle"/>
+
+            <vdf:field name="body">
+                <vdf:value>
+                    <div xmlns="http://www.w3.org/1999/xhtml">
+                        <p> Rogaland ungdomsmusikallag, som blei stifta i 2020, var Sander Slettebø Grøsfjell sin idé og tiltak. Han fekk med seg tidlegare medelevar frå Vågen videregående skole, pluss andre musikalske venner både frå Bjerkreim og andre plassar, med mål om å formidla musikalstoff i form av sceneoppsetjingar. </p>
+
+                        <p> I fjor sette gjengen opp ein kabaret på Undheim. I år, nærmare bestemt, torsdag 4. og fredag 5. august, blir det konsert på Bjerkreim samfunnshus. "Somehow a lot of musicals II - a musical concert" er tittelen på den musikalske opplevinga Sander og musikkvennene byr på. </p>
+
+                        <p> <a id="_6958c2ee-c3b1-42ab-9948-a2a1c64de159" href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/498400"> Sander Slettebø Grøsfjell </a> </p>
+
+                        <p> – Det blir ei blanding av musikalstoff som dei fleste kjenner godt til, som dei bare kjenner namnet på, og som dei kanskje ikkje kjenner i det heile tatt, lover Sander, som poengterer at konserten er resultatet av eit lagarbeid der kreative sjeler deler både idear og lærdom. </p>
+
+                        <p> Konsertane i neste veke blir faktisk Rogaland ungdomsmusikallag sine siste. </p>
+
+                        <p> – Laget vil bli oppløyst. Folka som har vore med, vil gå vidare med andre prosjekt. For min eigen del blir dette garantert ikkje det siste musikkteaterprosjektet eg er med på. Eg har eit mål og ein plan om å skriva noko sjølv. Musikalen min er eigentleg nokså klar, men foreløpig bare i hovudet mitt; eg har ikkje hatt tid til å få han ned på papiret ennå. Neste år eller om to år er eg kanskje klar med noko som presenterast for publikum; ein konsertversjon med fire skodespelarar og tre musikarar, røper han. </p>
+
+                        <p> <b> - Sjølv er du både skodespelar og musikar? </b> </p>
+
+                        <p> - I musikalen som eg skal få ut av hovudet, skal eg i all hovudsak vera pianist. På konsertane i neste veke er eg pianist, dirigent og songar. Men ein av songane klarer eg ikkje å gjera utan å ta med heile scenen, så derfor blir det ein liten monolog òg. Etter Bjerkreim brassband si siste oppsetjing, i 2019, la eg ut ei litt dum melding på Facebook om at eg var ferdig som skodespelar. Det var eg jo ikkje, viste det seg. </p>
+
+                        <p> <b> - Kven passar musikalkonsertane for? </b> </p>
+
+                        <p> - Musikalsjangeren er ein mellomting mellom høg- og lågkultur. Det er ikkje veldig komplekst og lett å forstå for dei aller, aller fleste. </p>
+
+                        <p> <img align="undefined" alt="undefined" id="_f0ed15f8-3942-4589-a522-4bfa7d0e581e" src="http://ece5.snapshot.api.no/dala/webservice/thumbnail/article/498402"/> </p>
+
+                        <p> <b> – Kva er favorittplassen din om sommaren? </b> </p>
+
+                        <p> – Eg trur det må vera Dyreparken. Det er så fint å gå rundt i den gode atmosfæren saman med - familien før, men nå - gode venner. Men mest av alt handlar forholdet mitt til Dyreparken om Sabeltann. Det er ein heilt fantastisk bra musikkteaterproduksjon av og med vanvitig flinke folk. Eg var stor fan då eg var liten, og er det ennå. </p>
+
+                        <p> <b> – Kva er det beste sommarminnet ditt? </b> </p>
+
+                        <p> – For nokon år sidan, då eg hadde fylt 18 og klart å stryka på oppkjøringa, kjørte eg og tre kameratar "Bjerkreim rundt". Éin av oss hadde lappen, eg og dei to andre hadde ikkje. Me fann på mange løgne sprell og hadde det utruleg gøy. </p>
+
+                        <p> <b> ... og det verste? </b> </p>
+
+                        <p> – Eg kjem ikkje på noko, og det er sikkert fordi eg bare har gode minne frå alle somrane eg har opplevd. </p>
+
+                        <p> <b> – Kva er det beste du kan få å eta om sommaren? </b> </p>
+
+                        <p> – Softis frå familiekafeen på Helleland </p>
+
+                        <p> <b> – Det blir ikkje sommar utan...? </b> </p>
+
+                        <p> – Softis frå Bens kafé </p>
+
+                        <p> <b> – Hytte ved sjøen eller på fjellet? </b> </p>
+
+                        <p> – Eg er ikkje noko fjellmenneske, så det måtte blitt ved sjøen - ikkje fordi eg er spesielt glad i å bada, men fordi eg liker havutsikt og avslapping. </p>
+
+                        <p> <b> – Saftis eller fløyteis? </b> </p>
+
+                        <p> – Fløyteis </p>
+
+                        <p> <b> – Kva vil du anbefala turistar å gjera i Dalane i sommar? </b> </p>
+
+                        <p> – Dei fire Dalane-kommunane er forskjellige, men byr totalt sett på mykje fint og mange flotte opplevingar. Derfor burde turistar sett av fire dagar - éin i kvar kommune - til å kjøra rundt og sjå, nyta og oppleva. </p>
+
+                        <p> <a target="_self" id="_1b450e65-665a-4692-af13-04bfb5f3db89" href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/495314"> Tar med seg teltet og Chevy: I ferien vil Helge sjå andre naturperler enn dei han ser til kvardags </a> </p>
+
+                        <p> <a target="_self" id="_5523f2d2-daa3-42e0-8f4f-979f0034d667" href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/496439"> Sexolog Margrethe (38) med klar anbefaling: – Bad naken i sommer! </a> </p>
+
+                    </div>
+                </vdf:value>
+            </vdf:field>
+
+            <vdf:field name="byline"/>
+            <vdf:field name="overline"/>
+            <vdf:field name="cxense-response"/>
+            <vdf:field name="hideForSearchEngines"> <vdf:value> false </vdf:value> </vdf:field>
+            <vdf:field name="hideInFeed"> <vdf:value> false </vdf:value> </vdf:field>
+            <vdf:field name="noFreeFlow"> <vdf:value> false </vdf:value> </vdf:field>
+            <vdf:field name="factsTitle"/>
+            <vdf:field name="factsText"/>
+            <vdf:field name="notShowInLatestNews"> <vdf:value> false </vdf:value> </vdf:field>
+            <vdf:field name="metacenter"/>
+            <vdf:field name="showOnFrontpageFrom"/>
+            <vdf:field name="comments"> <vdf:value> 1 </vdf:value> </vdf:field>
+            <vdf:field name="saxosyncstorystate"> <vdf:value> {"status":1,"statusText":"Eksportert ( Mon Jul 25 05:57:14 +00:00 )","systemMsg":"2022-07-25T05:57:14.595Z"} </vdf:value> </vdf:field>
+
+            <vdf:field name="com.escenic.tags"> <vdf:list/> </vdf:field>
+
+        </vdf:payload>
+
+    </content>
+
+    <title type="text"> Sander (21) er både arrangør og utøvar: - Eg la ut ei dum melding på Facebook </title>
+
+    <summary type="text"> Når Sander Slettebø Grøsfjell om halvanna veke inviterer til musikalkonsert i heimbygda, gjer han noko han for få år sidan sa han ikkje skulle gjera meir. </summary>
+
+    <link href="https://www.dalane-tidende.no.snap3.api.no/incoming/5-101-498373.html" rel="alternate"/>
+    <link href="http://ece5.snapshot.api.no/dala/webservice/escenic/publication/dalane-tidende/model/content-summary/story" rel="http://www.escenic.com/types/relation/summary-model" type="application/vnd.vizrt.model+xml" title="story"/>
+
+    <app:edited> 2022-07-25T06:52:59.000Z </app:edited>
+
+    <updated> 2022-07-25T01:00:00.000Z </updated>
+
+    <published> 2022-07-24T21:36:22.000Z </published>
+
+    <dcterms:available> 2022-07-25T01:00:00.000Z </dcterms:available>
+
+    <dcterms:created> 2022-07-24T18:25:46.000Z </dcterms:created>
+
+    <metadata:reference source="ece-auto-gen" sourceid="c22df9cd-dafe-4f5f-8d73-6c1ef8490187"/>
+
+    <link rel="http://www.vizrt.com/types/relation/home-section" href="http://ece5.snapshot.api.no/dala/webservice/escenic/section/6" title="Arkiv" type="application/atom+xml; type=entry"/>
+
+    <link rel="usage" href="http://ece5.snapshot.api.no/dala/webservice/usage/article/498373" type="text/uri-list" title="Incoming Links"/>
+
+    <link href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/498373/preview" rel="http://www.vizrt.com/types/relation/preview"/>
+
+    <link href="http://ece5.snapshot.api.no/dala/webservice/escenic/content/498373/log" rel="http://www.vizrt.com/types/relation/log"/>
+
+    <link href="http://ece5.snapshot.api.no/dala/webservice/open-search/tag-search-description.xml" rel="http://www.vizrt.com/types/relation/tag-search" title="tag-search-description" type="application/opensearchdescription+xml"/>
+
+    <link href="http://ece5.snapshot.api.no/dala/webservice/thumbnail/article/498411" rel="thumbnail" type="image/png"/>
+
+</entry>
+`
+
+}
+
+var xml = document.createElement("div")
+xml.innerHTML = getXml()
+console.log(xml)

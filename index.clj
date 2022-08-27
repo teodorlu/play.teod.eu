50223d210605b81c0aec228f3ed7016de40e038e
Teodor Heggelund <git@teod.eu>
2022-08-27 15:17:15 +0200

    Remove duplicate content

--- index.clj
+++ index.clj
@@ -116,15 +116,6 @@
       ""
       ]
 
-     ["** Stuff like this from other people:"
-      ""]
-     (let [org-link (fn [{:keys [name href]}]
-                      (str "[[" href "][" name "]]"))]
-       (for [{:keys [name href ]}
-             [{:name "Sindre's Random Ramblings" :href "https://play.sindre.me/"}
-              {:name "Kevin's WikiBlog" :href "https://kevin.stravers.net/"}]]
-         (str "- " (org-link {:name name :href href}) " (off-site link)")))
-
      ["** Comments? Hit me up."
       "Details on [[https://teod.eu][teod.eu]]."]
      [""

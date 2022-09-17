7a5c12f78181bda50335906700939bd46046925f
Teodor Heggelund <git@teod.eu>
2022-09-17 20:56:35 +0200

    Show an image

--- shades-of-deep-green/deep_green.clj
+++ shades-of-deep-green/deep_green.clj
@@ -1,2 +1,7 @@
 (ns deep-green
-  (:require [mikera.image.core :as imagez]))
+  (:require [mikera.image.core :as i]
+            [mikera.image.colours :as c]))
+
+(let [img (i/new-image 1920 1080)]
+  (i/fill! img (c/rand-colour))
+  (i/show img))

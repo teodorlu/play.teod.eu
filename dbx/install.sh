4860f641688bb9cac639e18188d5323649c8a151
Teodor Heggelund <git@teod.eu>
2022-09-26 22:15:32 +0200

    Think think

--- dbx/install.sh
+++ dbx/install.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env sh
 
 ln -srf ./dbxx.clj ~/.local/bin/dbxx
+
+# To install the shell wrapper, this must be sourced too:
+#
+#   function dbx() {
+#       dbxx "$@"
+#   }
+#
+# I use a shell wrapper because I eventually want a
+#
+#   dbx cd
+#
+# command.
+#
+#   dbx cd github-clone teodorlu/hotload
+#
+# man, this is extensible.
+#
+# aaand the embark function should /return/ the cd argument I think
+# or not.
+#
+# Hmm.

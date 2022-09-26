e9917b692472dd73487c069b83c75dbefbdb6028
Teodor Heggelund <git@teod.eu>
2022-09-26 18:20:19 +0200

    GET IT

--- dbx/dbxx.clj
+++ dbx/dbxx.clj
@@ -7,7 +7,8 @@
    [babashka.process :as process]
    [clojure.edn :as edn]
    [clojure.java.browse]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.pprint :refer [pprint]]))
 
 (def ^:dynamic *config-path* "~/.config/dbx/dbx.edn")
 
@@ -38,26 +39,52 @@
       str/trim))
 
 (defn provider-links [p]
-  (let [provider-config])
+  (when (fs/exists? (config-path))
+    (let [edn (edn/read-string (slurp (str (config-path))))]
+      (when-let [provider-method (get (:providers edn) p)]
+        #_
+        (prn provider-method)
+        (cond
+          ;; functions are interpreted "raw"
+          (contains? provider-method :fn)
+          (let [provider-fn (eval (:fn provider-method))]
+            (provider-fn))
+          :else nil
+
+          )
+
+
+        )
+      ))
 
   )
 
 (defn nav
   "Choose provider, then choose link"
-  [{}]
-  (let [p (fzf (providers))]
-    #_
-    (println p)
+  [{:keys [opts]}]
+  #_
+  (prn opts)
+  (let [p (or (:provider opts) (fzf (providers)))
+        links (provider-links p)
+        by-title-description (into {}
+                                   (for [l links]
+                                     [(str (:title l) " | " (:description l))
+                                      l]))
+        choice (fzf (for [l links]
+                      (str (:title l) " | " (:description l))))
+        ]
+
+    (pprint choice)
+    (pprint (get by-title-description choice))
 
 
-    (println "what now")
     )
 
   )
 
 (def dispatch-table
   [{:cmds ["provider"] :fn provider}
-   {:cmds ["nav"] :fn nav}])
+   {:cmds ["nav"] :fn nav :args->opts [:provider] :coerce {:provider :symbol}}])
 
 (defn main [& args]
   (cli/dispatch dispatch-table args {}))

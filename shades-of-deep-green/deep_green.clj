d9b575700661e11815900e9dff7b600b0af81d72
Teodor Heggelund <git@teod.eu>
2022-09-27 13:52:07 +0200

    deploy

--- shades-of-deep-green/deep_green.clj
+++ shades-of-deep-green/deep_green.clj
@@ -3,6 +3,9 @@
             [mikera.image.colours :as c]
             [img]))
 
-(let [img (img/new 1920 1080)]
-  (img/fill! img (img/rgb->color [0 200 0]))
+(let [img (img/new 1920 1080)
+      ;; color1 [0 200 0]
+      color2 [0 0.7 0.5]
+      ]
+  (img/fill! img (img/rgb->color color2))
   (img/show img))

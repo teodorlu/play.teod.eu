6b2546c1c7a6586c8758763c572e176f80b12857
Teodor Heggelund <git@teod.eu>
2022-09-29 19:16:27 +0200

    lookup random page

--- play.teod.eu-retrospective/scittle-buttons/buttons.cljs
+++ play.teod.eu-retrospective/scittle-buttons/buttons.cljs
@@ -1,8 +1,11 @@
 (ns buttons
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [ajax.core :refer [GET]]))
 
 (defn goto-random-page []
-
-  (prn "random page!!!"))
+  (let [handler (fn [big-index-str]
+                  (let [big-index (edn/read-string big-index-str)]
+                    (prn (rand-nth big-index))))]
+    (GET "/index/big.edn" {:handler handler})))
 
 (set! (.-goto_random_page js/window) goto-random-page)

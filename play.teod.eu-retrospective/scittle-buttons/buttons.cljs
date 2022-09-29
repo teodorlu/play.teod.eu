bafd346a5f45aa9469b7078cf5cd6f6168981838
Teodor Heggelund <git@teod.eu>
2022-09-29 19:06:21 +0200

    random page!!!

--- play.teod.eu-retrospective/scittle-buttons/buttons.cljs
+++ play.teod.eu-retrospective/scittle-buttons/buttons.cljs
@@ -3,8 +3,15 @@
 (require '[reagent.core :as r]
          '[reagent.dom :as rdom])
 
+#_
 (defn random-page-button [text]
   [:button text])
 
+#_
 (doseq [button (.querySelectorAll js/document "random-page-button")]
   (rdom/render [random-page-button "this is button?"] button))
+
+(defn goto-random-page []
+  (prn "random page!!!"))
+
+(set! (.-goto_random_page js/window) goto-random-page)

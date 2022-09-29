1f2d5bc6a441c6c0622526d9d6ab50c6b8036f50
Teodor Heggelund <git@teod.eu>
2022-09-29 18:39:22 +0200

    random page button

--- play.teod.eu-retrospective/buttons.cljs
+++ play.teod.eu-retrospective/buttons.cljs
@@ -1,3 +1,10 @@
 (ns buttons)
 
-(prn "hello, there :)")
+(require '[reagent.core :as r]
+         '[reagent.dom :as rdom])
+
+(defn random-page-button [text]
+  [:button text])
+
+(doseq [button (.querySelectorAll js/document "random-page-button")]
+  (rdom/render [random-page-button "this is button?"] button))

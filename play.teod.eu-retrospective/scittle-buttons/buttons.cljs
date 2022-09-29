42cebad14ab8f44e556a84ea4030af0bbee0c14c
Teodor Heggelund <git@teod.eu>
2022-09-29 19:21:29 +0200

    better button yay

--- play.teod.eu-retrospective/scittle-buttons/buttons.cljs
+++ play.teod.eu-retrospective/scittle-buttons/buttons.cljs
@@ -4,8 +4,11 @@
 
 (defn goto-random-page []
   (let [handler (fn [big-index-str]
-                  (let [big-index (edn/read-string big-index-str)]
-                    (prn (rand-nth big-index))))]
+                  (let [big-index (edn/read-string big-index-str)
+                        item (rand-nth big-index)]
+                    (prn item)
+                    ;; window.location.href = "/" + item.slug + "/"
+                    (set! (.-href js/window.location) (str "/" (:slug item) "/"))))]
     (GET "/index/big.edn" {:handler handler})))
 
 (set! (.-goto_random_page js/window) goto-random-page)

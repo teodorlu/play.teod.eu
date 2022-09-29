77e086898446bf0757576c176ffba8eec5577371
Teodor Heggelund <git@teod.eu>
2022-09-29 19:09:36 +0200

    up

--- play.teod.eu-retrospective/scittle-buttons/buttons.cljs
+++ play.teod.eu-retrospective/scittle-buttons/buttons.cljs
@@ -1,17 +1,6 @@
 (ns buttons
   (:require [clojure.edn :as edn]))
 
-(comment
-
-  (require '[reagent.core :as r]
-           '[reagent.dom :as rdom])
-
-  (defn random-page-button [text]
-    [:button text])
-
-  (doseq [button (.querySelectorAll js/document "random-page-button")]
-    (rdom/render [random-page-button "this is button?"] button)))
-
 (defn goto-random-page []
 
   (prn "random page!!!"))

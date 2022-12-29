ee38a97f57bc40b2bb55f1180eeb0e27ab8bf6f2
Teodor Heggelund <git@teod.eu>
2022-12-29 23:15:22 +0100

    More bencode examples

--- babashka-pods-for-beginners/explore/src/explore/bencode_examples.clj
+++ babashka-pods-for-beginners/explore/src/explore/bencode_examples.clj
@@ -24,3 +24,20 @@
 
 (bencode/decode "i99e")
 ;; => 99
+
+(defn examples [f & exs]
+  (for [e exs]
+    [e (f e)]))
+
+(examples bencode/encode
+          {"x" 1}
+          {"name" "tom"}
+          ["n" "tom"]
+          [1 2 3]
+          [1 2]
+          )
+;; => ([{"x" 1} "d1:xi1ee"]
+;;     [{"name" "tom"} "d4:name3:tome"]
+;;     [["n" "tom"] "l1:n3:tome"]
+;;     [[1 2 3] "li1ei2ei3ee"]
+;;     [[1 2] "li1ei2ee"])

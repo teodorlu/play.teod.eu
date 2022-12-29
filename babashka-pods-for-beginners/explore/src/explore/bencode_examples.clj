03ff5b22c2bad013341c9dd981df3f0979d2e08b
Teodor Heggelund <git@teod.eu>
2022-12-29 22:04:49 +0100

    Provide some bencode examples

---
+++ babashka-pods-for-beginners/explore/src/explore/bencode_examples.clj
@@ -0,0 +1,22 @@
+(ns explore.bencode-examples
+  (:require [explore.bencode :as bencode]))
+
+(bencode/encode {:foo "bar"})
+;; => "d3:foo3:bare"
+
+(= (bencode/encode {:foo "bar"})
+   (bencode/encode {"foo" "bar"}))
+;; => true
+
+(bencode/encode "teodor")
+;; => "6:teodor"
+
+(bencode/encode 99)
+;; => "i99e"
+
+(bencode/encode 1.2)
+;; => error - integers only!
+
+(bencode/decode "i99e")
+;; => 99
+:rcf

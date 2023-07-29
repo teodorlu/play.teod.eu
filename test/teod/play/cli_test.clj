766a57719295e07c839965ab7296ed4413315df5
Teodor Heggelund <git@teod.eu>
2023-07-29 15:29:37 +0200

    I am the walrus

--- test/teod/play/cli_test.clj
+++ test/teod/play/cli_test.clj
@@ -6,3 +6,10 @@
 (deftest words-test
   (is (= "i am the walrus"
          (play.cli/words "i" "am" "the" "walrus"))))
+
+(deftest lines-test
+  (is (= "i
+am
+the
+walrus"
+         (play.cli/lines "i" "am" "the" "walrus"))))

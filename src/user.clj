2c8bf1b93bfc83b067535e1e8c500b395dead24e
Teodor Heggelund <git@teod.eu>
2024-02-13 20:25:37 +0100

    Clerk start code

---
+++ src/user.clj
@@ -0,0 +1,7 @@
+(ns user)
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn clerk-start! []
+  (let [clerk-serve (requiring-resolve 'nextjournal.clerk/serve!)
+        clerk-port 7770]
+    (clerk-serve {:browse? true :port clerk-port})))

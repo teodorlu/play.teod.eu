ebde8667685d0e3f1fdafdc68c930800285ffc55
Teodor Heggelund <git@teod.eu>
2024-02-25 16:00:17 +0100

    Explore doc metadata

---
+++ src/teod/play/understand2.clj
@@ -0,0 +1,48 @@
+(ns teod.play.understand2
+  (:require
+   [nextjournal.clerk :as clerk]
+   [nextjournal.clerk.viewer :as v]
+   [teod.play.cli :as cli]))
+
+;; continue the understanding-building!
+
+(defn big-table [x]
+  (v/with-viewer (dissoc v/table-viewer :page-size)
+    x))
+
+(defn relations []
+  (->> (cli/files->relations {})
+       vals
+       (sort-by :slug)))
+
+#_
+^{::clerk/width :full}
+(big-table
+ (->> (cli/files->relations {})
+      vals
+      (sort-by :slug)))
+
+^{::clerk/width :full}
+(big-table
+ (->> (relations)
+      (take 4)))
+
+(clerk/table
+ (->> (relations)
+      (filter (fn [rel]
+                (re-matches #".*Simple.*" (:title rel))))))
+
+;; I currently do not have any metadata for:
+;;
+;; - The fact that Simple Made Easy is by Rich Hickey
+;; - The fact that Simple Made Easy is a talk with a youtube link
+
+;; How about `:author/uuid` ... ?
+
+;; This is when I probably want a datascript database of my stuff.
+;; ... and I (think) I want to keep my current file storage system, just add an index.
+
+
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:height "50vh"}}])

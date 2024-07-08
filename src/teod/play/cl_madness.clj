f5e345da95c52870e695a665a49531c9e7ed0532
Teodor Heggelund <git@teod.eu>
2024-07-08 16:52:53 +0200

    More cl madness

--- src/teod/play/cl_madness.clj
+++ src/teod/play/cl_madness.clj
@@ -1,4 +1,6 @@
-(ns teod.play.cl-madness)
+(ns teod.play.cl-madness
+  (:require
+    [clojure.string :as str]))
 
 (defmacro gen-c*r [c*r]
   (let [c*r-str (str c*r)]
@@ -19,15 +21,38 @@
 (lflast '(junk ((ignore 5) garbage)))
 ;; => 5
 
-(do
-  (defn gen-frl
-    "Bring the joy of _cadadddr_ to Clojure, but Clojure-flavoured"
-    [frl]
-    (assert (symbol? frl))
-    (let [frl-str (str frl)]
-      (cond (= "first" frl-str) first
-            (= "last" frl-str) last)))
+(defn gen-frl
+  "Bring the joy of _cadadddr_ to Clojure, but Clojure-flavoured"
+  [frl]
+  (assert (symbol? frl))
+  (let [frl-str (str frl)
+        full {"first" first "last" last "rest" rest "butlast" butlast}
+        short {\f first \l last \r rest \b butlast}
+        ending (cond (str/ends-with? frl-str "butlast") "butlast"
+                     (str/ends-with? frl-str "first") "first"
+                     (str/ends-with? frl-str "last") "last"
+                     (str/ends-with? frl-str "rest") "rest")]
+    (assert (contains? full ending))
+    (let [frl-rest (subs frl-str 0 (- (count frl-str) (count ending)))]
+      (println frl-rest)
+      (assert (every? #(contains? short %) frl-rest))
+      (reduce comp (full ending) (map short frl-rest)))))
 
-  (let [l '(1 2 3 (4 5))]
-    (assert (= ((eval 'last) l)
-               ((gen-frl 'last) l)))))
+((gen-frl 'bbbutlast)
+ '(1 2 3 (4 ((5)))))
+;; => (1)
+
+((gen-frl 'llast)
+ '(1 2 3 (4 5)))
+;; => 5
+
+((gen-frl 'llllast)
+ '(1 2 3 (4 ((5)))))
+;; => 5
+
+
+(let [l '(1 2 3 (4 5))]
+  (assert (= ((eval 'last) l)
+             ((gen-frl 'last) l))))
+
+(comp ~@(map {\a first \d rest} "ad"))

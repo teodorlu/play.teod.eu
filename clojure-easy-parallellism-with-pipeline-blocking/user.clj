bc4fdec38b2928750408996bed304615a7ce4d81
Teodor Heggelund <git@teod.eu>
2024-07-12 14:05:36 +0200

    Rebuild from local source

--- clojure-easy-parallellism-with-pipeline-blocking/user.clj
+++
@@ -1,7 +0,0 @@
-(ns user)
-
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn clerk-start! []
-  (let [clerk-serve (requiring-resolve 'nextjournal.clerk/serve!)
-        clerk-port 7297]
-    (clerk-serve {:browse? true :port clerk-port})))

98cad55675c489b102c102e4cdb4d21ddf07fc12
Teodor Heggelund <git@teod.eu>
2024-07-13 14:54:20 +0200

    eval2, apply2

--- src/tplay/iki.clj
+++ src/tplay/iki.clj
@@ -16,3 +16,42 @@
         conn (d/create-conn datascript-schema)]
     (d/transact! conn rels)
     @conn))
+
+;; need to declare `apply2` before referring to it from `eval2`, otherwise it won't work!
+
+(declare apply2)
+
+(defn eval2 [form]
+  (cond (= '() form)
+        ()
+
+        (= '+ form)
+        +
+
+        (= '- form)
+        -
+
+        (= '* form)
+        *
+
+        (number? form)
+        form
+
+        (seq? form)
+        (apply2 (eval2 (first form))
+                (map eval2 (rest form)))))
+
+(defn apply2 [& forms]
+  (let [explicit-args (butlast forms)
+        variadic-args (last forms)
+        operator (first explicit-args)
+        args (concat (rest explicit-args) variadic-args)]
+    ;; need to cheat here, I don't know how to implement this part of apply.
+    (apply operator args)))
+
+(eval2 '(+ 1 2 3))
+;; => 6
+
+(apply + '(1 2 3))
+;; => 6
+

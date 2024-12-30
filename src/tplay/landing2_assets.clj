6eb6643b523b0560121454979a8bdfcc979e6e09
Teodor Heggelund <git@teod.eu>
2024-12-30 17:25:12 +0100

    Delete unused code

--- src/tplay/landing2_assets.clj
+++
@@ -1,4 +0,0 @@
-(ns tplay.landing2-assets)
-
-(def pandoc-css-link [:link {:rel :stylesheet :href "/src/tplay/landing2-pandoc.css"}])
-(def favicon-link [:link {:rel :icon :type "image/x-icon" :href "/green.png"}])

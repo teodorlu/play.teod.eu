f2fc07020e006b0ac9be783d5bd423fb5873f81a
Teodor Heggelund <git@teod.eu>
2025-10-01 18:57:42 +0200

    Kondo for app devs: Push what I got in order to make a repro

--- kondo-hooks-for-application-programmers/kondo_hooks_for_application_programmers.clj
+++ kondo-hooks-for-application-programmers/kondo_hooks_for_application_programmers.clj
@@ -1,5 +1,12 @@
-^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
-(ns kondo-hooks-for-application-programmers)
+^{:nextjournal.clerk/visibility {:code :fold :result :hide}}
+(ns kondo-hooks-for-application-programmers
+  {:nextjournal.clerk/toc true
+   :clj-kondo/config
+   '{:hooks
+     {:analyze-call
+      {kondo-hooks-for-application-programmers/defgrinning
+       kondo-hooks-for-application-programmers/defgrinning-hook}}}}
+  (:require [clojure.string :as str]))
 
 ;; # CLJ-Kondo hooks for application programmers
 
@@ -8,7 +15,7 @@
 ;; Static analysis with CLJ-Kondo can help tame that confusion and avoid those tears.
 ;; Let's learn about just that.
 
-;; ## Kondo already knows certain macros
+;; ## Kondo knows certain macros
 
 ;; Take the `for` macro:
 
@@ -54,32 +61,104 @@
 
 (capture-out (macroexpand '(deflaughing the-number-after-one-and-two (+ 1 2))))
 
-;; More importantly, the macro will give an "unresolved symbol" warning with
-;; CLJ-Kondo-powered tooling:
+;; *But Kondo does not understand your macro!*
 
 (deflaughing laugh-all-you-want (+ 1 2))
 
-(clerk/html [:img {:src "https://play.teod.eu/kondo-hooks-for-application-programmers/kondo-warning.png"}])
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(defn img [src]
+  (clerk/html [:img {:src (str "https://play.teod.eu/kondo-hooks-for-application-programmers/" src)}]))
+
+;; "unresolved symbol", it says!
+^{:nextjournal.clerk/visibility {:code :hide :result :show}}
+(img "kondo-does-not-understand-it.png")
+
+;; ## Kondo analyze hooks
+;;
+;; Hooks!
+;; The source of truth for their usage is [doc/hooks.md] in the Kondo repo.
+;; Lets try those hooks.
+;;
+;; [doc/hooks.md]: https://github.com/clj-kondo/clj-kondo/blob/master/doc/hooks.md
+
+;; Hooks work on a custom type tailored to represent Clojure code.
+;; Plain symbols and Clojure data structures because it doesn't keep track of location data: which column, which line.
+
+
+^{:nextjournal.clerk/visibility {:code :show :result :hide}}
+(require '[clj-kondo.hooks-api :as hooks])
+(def node
+  (hooks/list-node [(hooks/string-node "hi")
+                    (hooks/string-node ", there!")]))
+
+(type node)
+
+;; I'm guessing that node can be str-ed back to Clojure code ...
+
+(str node)
+
+;; bingo.
+;; Now we can get our delicious Clojure data structures back.
+
+(read-string (str node))
+
+;; Okay, nice.
+;; How do we build support for our new macro?
+;; The API docs give us a clue:
+;;
+;; > It is not important that the code is rewritten exactly according to the
+;; > macroexpansion. What counts is that the transformation rewrites into code
+;; > that clj-kondo can understand.
+;;
+;; Nice!
+;; Our task is to rewrite the code into code that Kondo *can* understand.
+;;
+;; Let's try expand `(deflaughing x (+ 1 2))` into `(def x (+ 1 y))`.
+
+;; first a macro.
+
+(defmacro defgrinning [sym & body]
+  (println (str sym "😁"))
+  `(def ~sym (do ~@body)))
+
+(with-out-str (macroexpand '(defgrinning x 33)))
+
+;; Then a hook!
+
+(defn defgrinning-hook [{:keys [node]}]
+  (let [[_defgrinning sym & body] (:children node)]
+    (when-not sym
+      (throw (ex-info "No sym provided, you silly person!" {:sym sym :body body})))
+    (when-not (= sym (symbol "☢"))
+      (throw (ex-info "Radioactivity disallowed, it does not cause grinning!"
+                      {:sym sym :body body})))
+
+    {:node (hooks/list-node (list*
+                             (hooks/token-node 'def)
+                             (hooks/token-node sym)
+                             body))}))
+
+;; I'm going to cheat a bit here, because I've already "installed" this hook in
+;; my namespace declaration.
+
+^{:nextjournal.clerk/visibility {:code :fold :result :show} :nextjournal.clerk/no-cache true}
+(clerk/caption "Beware, a listing of code, outside of top-down flow"
+               (clerk/code
+                (->> (slurp *file*)
+                     (str/split-lines)
+                     (take 9)
+                     (str/join "\n"))))
+
+(defgrinning x "it's fine ...")
 
 
 
 
-;; Already, I've noticed two problems:
-;; 1. CLJ-Kondo gives me an "unresolved symbol" warning for `laugh-all-you-want`
-;; 2. Clerk's dependency analysis breaks down, because it doesn't guess that redefining
-;;    `deflaughing` in the background needs to invalidate the capture-out of the macroexpand.
-
-
-
-{:nextjournal.clerk/visibility {:code :show :result :show}}
 
 ^{:nextjournal.clerk/visibility {:code :hide :result :show}}
 (clerk/html [:div {:style {:height "30vh"}}])
 
-
-
 ^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
-
 (comment
  ((requiring-resolve 'nextjournal.clerk/serve!) {:browse true})
  ((requiring-resolve 'clojure.repl.deps/sync-deps))

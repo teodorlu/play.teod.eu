db1f4fb4e49a8bac21dc47b519eb04ce58260c14
Teodor Heggelund <git@teod.eu>
2026-01-15 08:45:15 +0100

    rss???

---
+++ src/tplay/rss.clj
@@ -0,0 +1,28 @@
+(ns tplay.rss)
+
+;; I don't need no makefile
+;; I don't need no task
+;; I just need a REPL eval
+;; For now
+
+;; 🎵 doin' things that don't scale 🎵
+;; 🎵 for texture! 🎵
+
+;; What brought us here?
+;; I stumbled over https://aartaka.me/pidgin.html by Artyom Bologov yesterday.
+;; I liked what I saw so much that I had to send an email!
+;; Later the same evening, I got a super-pleasant reply.
+;; He had read my poetry 😂
+;; And asked for an RSS feeed.
+
+;; I *have* been RSS curious, so why not.
+
+;; But I have a problem.
+;; I have piles of drafts live on the web, that's not meant for anybody.
+;; Additionally, I mix Enlish and Norwegian.
+;; My solution: tailored RSS feeds.
+;; I'll start with english-ready-for-comments.rss
+
+(comment
+
+  )
